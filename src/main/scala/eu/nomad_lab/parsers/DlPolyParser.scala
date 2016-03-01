28c401e41e482d542081f9371b598739e3252118
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-03-01 18:38:24 +0100

    Add parserId and versionInfo in parserInfo

--- src/main/scala/eu/nomad_lab/parsers/DlPolyParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/DlPolyParser.scala
@@ -1,12 +1,22 @@
 package eu.nomad_lab.parsers
+
+import eu.{nomad_lab=>lab}
 import eu.nomad_lab.DefaultPythonInterpreter
 import org.{json4s => jn}
+import scala.collection.breakOut
 
 object DlPolyParser extends SimpleExternalParserGenerator(
   name = "DlPolyParser",
   parserInfo = jn.JObject(
     ("name" -> jn.JString("DlPolyParser")) ::
-      ("version" -> jn.JString("1.0")) :: Nil),
+      ("parserId" -> jn.JString("DlPolyParser" + lab.DlPolyVersionInfo.version)) ::
+      ("versionInfo" -> jn.JObject(
+        ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
+          (lab.DlPolyVersionInfo.toMap.map{ case (key, value) =>
+            (key -> jn.JString(value.toString))
+          }(breakOut): List[(String, jn.JString)])
+      )) :: Nil
+  ),
   mainFileTypes = Seq("text/.*"),
   mainFileRe = """ DL_POLY """.r,
   cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/dl_poly/parser/parser-dl_poly/SimpleDL_POLYParser.py",
